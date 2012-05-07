d67c654c5fcce72c74d5a9d022902f520d7a7a27
franks42 <frank.siebenlist@gmail.com>
2012-05-07 11:54:45 -0700

    added license info to project.clj

--- project.clj
+++ project.clj
@@ -1,5 +1,8 @@
 (defproject clj-info "0.2.4-SNAPSHOT"
   :description "Enhanced clojure doc-info facility for use in REPL and beyond."
+  :url "https://github.com/franks42/clj-info"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [hiccup "0.3.8"]]
  	:dev-dependencies [[lein-marginalia "0.6.0"]
