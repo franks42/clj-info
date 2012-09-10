8bee6796e938d253fa70cf81d292251b6c26440d
franks42 <frank.siebenlist@gmail.com>
2012-09-09 22:17:46 -0700

    bumped versions in project.clj to latest and greatest

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject clj-info "0.2.4-SNAPSHOT"
+(defproject clj-info "0.2.4"
   :description "Enhanced clojure doc-info facility for use in REPL and beyond."
   :url "https://github.com/franks42/clj-info"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.3.0"]
-                 [hiccup "0.3.8"]]
- 	:dev-dependencies [[lein-marginalia "0.6.0"]
- 	                   [codox "0.5.0"]]
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [hiccup "1.0.1"]]
+ 	:dev-dependencies [[lein-marginalia "0.7.1"]
+ 	                   [codox "0.6.1"]]
  	:main clj-info)
