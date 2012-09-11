745b4bb35949eeb63528fe94513204b5ad86ba49
franks42 <frank.siebenlist@gmail.com>
2012-09-11 14:53:29 -0700

    0.2.6 bugged by some dep-corruption - reinstall lein worked (?)

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-info "0.2.6-SNAPSHOT"
+(defproject clj-info "0.2.6"
   :description "Enhanced clojure doc-info facility for use in REPL and beyond."
   :url "https://github.com/franks42/clj-info"
   :license {:name "Eclipse Public License"
@@ -6,6 +6,6 @@
   :dependencies [[org.clojure/clojure "1.4.0"]
                  ;;[hiccup "0.3.8"]]
                  [hiccup "1.0.1"]]
- 	:dev-dependencies [[lein-marginalia "0.7.1"]
- 	                   [codox "0.6.1"]]
+;;  	:dev-dependencies [[lein-marginalia "0.7.1"]
+;;  	                   [codox "0.6.1"]]
  	:main clj-info)
