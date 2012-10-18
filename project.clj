f8b9753eaa20592577a581b692b127541cab33f9
franks42 <frank.siebenlist@gmail.com>
2012-10-17 22:45:45 -0700

    0.3.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-info "0.3.1-SNAPSHOT"
+(defproject clj-info "0.3.1"
   :description "Enhanced clojure doc-info facility for use in REPL and beyond."
   :url "https://github.com/franks42/clj-info"
   :license {:name "Eclipse Public License"
